c11851afbca95f61cfcd4a31aba2c94a1a219bc9
Fouzi Matar <fouzi.matar@gmail.com>
2020-04-25 02:28:20 +0200

    PHP truthy falsy

---
+++ php-projects/truthy-falsy/truthy-falsy.php
@@ -0,0 +1,13 @@
+if ("") {
+  echo "this will not print";
+} elseif (null) {
+  echo "this will not print";
+} elseif ([]) {
+  echo "this will not print";
+} elseif (0) {
+  echo "this will not print";
+} elseif ("0") {
+  echo "this will not print";
+} else {
+  echo "Finally!";
+}
